4e2e2389822570e935a43bc515a32ea3e5683927
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-06-11 13:06:51 -0400

    link

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoScraper'
+  database: process.env.MONGODB_URI || 'mongodb://heroku_hkq48n0m:i4hcm5t63rmo7ofcni4es5dmlj@ds255740.mlab.com:55740/heroku_hkq48n0m'
 };
